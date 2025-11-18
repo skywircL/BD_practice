f223170d2ad7d4bbee5b2ed703bd2671b98e55ce
skywircL <1802515800@qq.com>
2025-11-18 19:40:41 +0800

    format

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-tokenizer = BertTokenizer.from_pretrained('./bert')
+tokenizer = AutoTokenizer.from_pretrained('./bert')
 model = AutoModelForSequenceClassification.from_pretrained('./bert')
 
 class Dataset(torch.utils.data.Dataset):
