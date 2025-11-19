0a11dae8392098680cab0d67edb001e0bdfb6266
skywircL <1802515800@qq.com>
2025-11-19 21:01:10 +0800

    use bf16

--- train.py
+++ train.py
@@ -76,7 +76,7 @@ training_args = TrainingArguments(
     learning_rate=3e-5,
     warmup_steps=200,
     weight_decay=1e-4,
-
+    bf16=True,
     dataloader_num_workers=4,
     report_to="none",                  # 不推送到 wandb
     torch_compile=False,
